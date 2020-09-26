28607f5d223a0f000357a60054e8b0cc8c98df7e
Crowdbotics <team@crowdbotics.com>
2020-09-26 03:28:19 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList111095', human_name: 'Article List', access_route: 'ArticleList111095'},
 { name: 'Maps111082', human_name: 'Maps', access_route: 'Maps111082', icon: 'map'},
 { name: 'SignUp21111077', human_name: 'Sign Up 2 1', access_route: 'SignUp21111077'},
 { name: 'SignUp22111076', human_name: 'Sign Up 2 2', access_route: 'SignUp22111076'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import ArticleList111095Navigator from '../features/ArticleList111095/navigator';
 import Maps111082Navigator from '../features/Maps111082/navigator';
 import SignUp21111077Navigator from '../features/SignUp21111077/navigator';
 import SignUp22111076Navigator from '../features/SignUp22111076/navigator';
@@ -16,6 +17,7 @@ import SignUp22111076Navigator from '../features/SignUp22111076/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+ArticleList111095: { screen: ArticleList111095Navigator },
 Maps111082: { screen: Maps111082Navigator },
 SignUp21111077: { screen: SignUp21111077Navigator },
 SignUp22111076: { screen: SignUp22111076Navigator },
